7d8e9db3b27493f85c1c5b41cf0857156969f95a
Arijit Das <arijitdas2k3@gmail.com>
2025-10-08 08:52:01 +0530

    updated

---
+++ Climafix/components/script.js
@@ -0,0 +1,20 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+  const dropBtn = document.querySelector(".dropbtn");
+  const dropdownContent = document.querySelector(".dropdown-content");
+
+  if (dropBtn && dropdownContent) {
+    dropBtn.addEventListener("click", (event) => {
+      event.preventDefault();
+      dropdownContent.classList.toggle("show");
+    });
+
+    window.addEventListener("click", (e) => {
+      if (!e.target.matches(".dropbtn")) {
+        if (dropdownContent.classList.contains("show")) {
+          dropdownContent.classList.remove("show");
+        }
+      }
+    });
+  }
+});
